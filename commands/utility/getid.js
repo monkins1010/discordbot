f439b186fe7f8e397c3a008183bdbb4358288461
Chris <34682781+monkins1010@users.noreply.github.com>
2024-02-07 14:45:40 +0000

    wait for file create

--- commands/utility/getid.js
+++ commands/utility/getid.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder, EmbedBuilder, AttachmentBuilder  } = require('discord.js');
 const { getdeeplink, getTinyUrl } = require("../../utils/getdeeplink.js")
 const { checkDiscordUserExists, setDiscordUsers } = require("../../utils/database.js")
+const wait = require('node:timers/promises').setTimeout;
 const qr = require('qr-image');
 const fs = require('fs');
 
@@ -19,7 +20,8 @@ module.exports = {
 		const tinyurl = await getTinyUrl(deeplinkurl);
 		var qr_png = qr.image(deeplinkurl, { type: 'png' });
 		const pngname = `${interaction.user.id}qr_code.png`;
-		qr_png.pipe(fs.createWriteStream("./public/" + pngname));
+		await qr_png.pipe(fs.createWriteStream("./public/" + pngname));
+		await wait(2_000);
 		const file = new AttachmentBuilder("./public/" + pngname);
 		
 		await interaction.editReply({ embeds: [templateEmbed(pngname, tinyurl)], files: [file] });
